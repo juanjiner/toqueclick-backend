a1297dbc666031390003d7fed54f780700020fc7
Masealvi <malbercavilchez@gmail.com>
2026-03-25 23:26:26 -0500

    log de conexion a DB

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,22 +5,22 @@ let pool: Pool;
 
 export const getPool = () => {
     if (!pool) {
+        const object = {
+            host: process.env.DB_HOST,
+            port: Number(process.env.DB_PORT),
+            user: process.env.DB_USER,
+            password: `${process.env.DB_PASSWORD}`,
+            database: process.env.DB_NAME,
+            max: 1, // importante para Lambda
+            idleTimeoutMillis: 0,
+            connectionTimeoutMillis: 2000,
+            ssl:
+                process.env.DB_SSL === "true"
+                    ? { rejectUnauthorized: false }
+                    : false,
+        }
+        logger.info("ver datos de conexion: ", object);
         try {
-            const object = {
-                host: process.env.DB_HOST,
-                port: Number(process.env.DB_PORT),
-                user: process.env.DB_USER,
-                password: `${process.env.DB_PASSWORD}`,
-                database: process.env.DB_NAME,
-                max: 1, // importante para Lambda
-                idleTimeoutMillis: 0,
-                connectionTimeoutMillis: 2000,
-                ssl:
-                    process.env.DB_SSL === "true"
-                        ? { rejectUnauthorized: false }
-                        : false,
-            }
-            logger.info("ver datos de conexion: ", object);
             pool = new Pool(object);
         } catch (error) {
             logger.error(error);
