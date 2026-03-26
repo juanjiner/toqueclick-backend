aa998560302121f9d8f4d80a271c101df07dd886
Masealvi <malbercavilchez@gmail.com>
2026-03-26 00:22:30 -0500

    log de conexion a DB

--- src/modules/cities/city.repository.ts
+++ src/modules/cities/city.repository.ts
@@ -1,18 +1,17 @@
 import { getPool } from "../../config/database.js";
 import { Departament, Province, District } from "./city.model.js";
 
-const pool = getPool();
 
 export class CityRepository {
 
     async findDepartaments(): Promise<Departament[]> {
-        const result = await pool.query(
+        const result = await getPool().query(
             "SELECT id, departament FROM maestro.departament ORDER BY departament");
         return result.rows;
     }
 
     async findProvincesByDepartament(department: string): Promise<Province[]> {
-        const result = await pool.query(
+        const result = await getPool().query(
             "SELECT id, province FROM maestro.province WHERE ccdd= $1 ORDER BY province",
             [department]
         );
@@ -20,7 +19,7 @@ export class CityRepository {
     }
 
     async findDistrictsByProvince(province: string): Promise<District[]> {
-        const result = await pool.query(
+        const result = await getPool().query(
             "SELECT id, district FROM maestro.district WHERE ccpp= $1 ORDER BY district",
             [province]
         );
