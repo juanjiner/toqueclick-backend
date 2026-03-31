e0b0943bea0151a33be86ba062b56bdbd9e6f437
Masealvi <malbercavilchez@gmail.com>
2026-03-31 07:10:10 -0500

    cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,7 +1,7 @@
 import cors from "cors";
 
 export const corsConfig = cors({
-    origin: ["http://localhost:4200", "https://toqueclick.com/"],
+    origin: ["http://localhost:4200", "https://toqueclick.com"],
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
     credentials: true,
 });
