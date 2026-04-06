d96394586350b610df6b371e3c92b193520bad8c
Masealvi <malbercavilchez@gmail.com>
2026-04-06 09:01:49 -0500

    dominios

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,7 +1,7 @@
 import cors from "cors";
 
 export const corsConfig = cors({
-    origin: ["http://localhost:4200", "https://toqueclick.com"],
+    origin: ["http://localhost:4200", "https://toqueclick.com", "https://toquefacil.net", "https://toquepay.com.pe", "https://toqueapp.pe"],
     methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
     credentials: true,
 });
